5f3645d11958b5d86ecdc9cc9acd581d14e3fa7a
nadikkk <109106413+nadikkk@users.noreply.github.com>
2023-02-28 11:00:18 +0200

    add5

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -4,7 +4,7 @@ import css from './Button.module.css';
 export default function Button({onClick}) {
   return (
     
-        <button type="button" className={css.button} onClick={onClick}>
+        <button type="button" className={css.button} onClick={()=>{onClick()}}>
           Load more
         </button>
 
